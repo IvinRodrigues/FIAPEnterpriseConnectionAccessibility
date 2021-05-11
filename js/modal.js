a653c5c23e65f7464a078b63564f8a769ad730ae
IvinRodrigues <ivinrodriguees@gmail.com>
2021-05-10 21:18:41 -0300

    Correção do zoom

--- js/modal.js
+++ js/modal.js
@@ -53,7 +53,7 @@ function alterarAcessibilidade(elemento) {
         if (state == true) {
 
             localStorage.setItem('daltonismo', true)
-            body.style.zoom = '160%'
+            body.style.zoom = '130%'
 
         } else {
 
